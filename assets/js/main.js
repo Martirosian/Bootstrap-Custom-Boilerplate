9562e79ebbaeb8b35a6a7fe3025b3af229540fd2
Martirosian <rmartirosian94@gmail.com>
2016-10-01 13:37:30 +0400

    First commoit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var BLPT = {};
+
+BLPT.ui = {};
+
+$( function() {
+
+	var settings = {
+		navbarSlide     : true,
+
+	};
+
+	BLPT.ui.config()
+	BLPT.ui.init(settings.navbarSlide);
+});
+
+BLPT.ui.config = function() {
+
+};
+
+BLPT.ui.init = function() {
+
+};
